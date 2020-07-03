1b36315dcf2e163d1df53b5a1b06ab08dbeb98a6
Egor <ivashin.egor98@gmail.com>
2020-07-03 14:08:41 +0300

    f

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-=======
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Blog from './Blog'
 import * as serviceWorker from './serviceWorker'
->>>>>>> egor
 
 ReactDOM.render(
   <React.StrictMode>
