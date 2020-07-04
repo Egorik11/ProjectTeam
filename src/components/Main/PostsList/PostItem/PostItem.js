2943db6c2f030fa4b9c63fb46c38b5900426de6e
Egor <ivashin.egor98@gmail.com>
2020-07-04 17:15:26 +0300

    post

--- src/components/Main/PostsList/PostItem/PostItem.js
+++ src/components/Main/PostsList/PostItem/PostItem.js
@@ -1,26 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function PostItem() {
+  const [postTitle, setPostTitle] = useState([]);
+  useEffect(async () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setPostTitle(data);
+      });
+  }, []);
+
   return (
     <div>
-      <div className="uk-card uk-card-default uk-margin-medium-bottom">
-        <div className="uk-card-header">
-          <h3 className="uk-card-title uk-margin-remove-bottom uk-flex uk-flex-middle uk-flex-between">
-            Title <a href="/" className="uk-icon-link" uk-icon="heart" />
-          </h3>
-        </div>
-        <div className="uk-card-body">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt.
-          </p>
-        </div>
-        <div className="uk-card-footer">
-          <a href="post.html" className="uk-button uk-button-text">
-            Read more
-          </a>
-        </div>
-      </div>
+      {postTitle.map((post) => {
+        return (
+          <div
+            key={post.id}
+            className="uk-card uk-card-default uk-margin-medium-bottom"
+          >
+            <div className="uk-card-header">
+              <h3 className="uk-card-title uk-margin-remove-bottom uk-flex uk-flex-middle uk-flex-between">
+                {post.title}
+                <a href="/" className="uk-icon-link" uk-icon="heart" />
+              </h3>
+            </div>
+            <div className="uk-card-body">
+              <p>{post.body}</p>
+            </div>
+            <div className="uk-card-footer">
+              <a href="post.html" className="uk-button uk-button-text">
+                Read more
+              </a>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -3,6 +3,22 @@ import Header from '../../components/Header/Header'
 import Main from '../../components/Main/Main'
 
 export default class Blog extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      favorites: [],
+      posts: [],
+      limit:12, 
+      order : "asc",
+      view : "grid",
+      page : 1,
+      total : 100,
+      loading : false,
+      error : null,
+    }
+  }
+  
   render() {
     return (
       <>
